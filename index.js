630abd77d9c47db5e6569b501bb82a8e3f33660c
Riscorn <1969555215@qq.com>
2025-01-12 17:04:43 +0800

    Changes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 load_element("action-ctrl")
 load_element("action-label")
+document.addEventListener("DOMContentLoaded", ()=>{
+    const i=document.querySelector("#checkbox");
+    const j=document.querySelector("#theme-link");
+    i.addEventListener("change",()=>{
+        console.log(i.checked,j);
+        if(i.checked){
+            $set_attr(j,"href","elegant/elegant-theme/theme.css");
+        }else{
+            $remove_attr(j,"href");
+        }
+    })
+});
